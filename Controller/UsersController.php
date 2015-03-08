1bd8afba540e9bcafcc44a123ffd3b61c5e99f9b
butchigho <butchigho93@gmail.com>
2015-03-08 17:19:39 +0700

    user

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -71,7 +71,83 @@ class UsersController extends AppController {
 		$this->User->recursive = 0;
 		$this->set('users', $this->Paginator->paginate());
 	}
-
+/**
+ * admin_index
+*/
+public function admin_index() {
+	$this->User->recursive = 0;
+		$this->set('users', $this->Paginator->paginate());
+	}
+public function admin_add() {
+	if ($this->request->is('post')) {
+		$infoUser = $this->request->data;
+		//kiem tra su ton tai cua user
+		$infoExit = $this->User->find('all',array(
+			'conditions'=>array('User.username'=>$infoUser['User']['username'])
+			));
+		//debug($infoExit);
+			if(empty($infoExit)){
+				$password = $this->request->data['User']['password'];
+				$infoUser['User']['password'] = md5($password);
+				//debug($infoUser);
+					$this->User->create();
+					if ($this->User->save($infoUser)) {
+						$this->Session->setFlash('Tạo mới thành công','default',array('class'=>'alert alert-info'),'exitUser');
+						return $this->redirect(array('controller'=>'Rooms','action' => 'admin_index'));
+					} else {
+						$this->Session->setFlash(__('The user could not be saved. Please, try again.'));
+					}
+			}else{
+				$this->Session->setFlash('Người dùng đã tồn tại','default',array('class'=>'alert alert-danger'),'exitUser');
+				$this->redirect($this->referer());
+			}
+		
+		}
+		$groups = $this->User->Group->find('list');
+		$this->set(compact('groups'));
+	}
+public function admin_view($id=null) {
+	if (!$this->User->exists($id)) {
+			throw new NotFoundException(__('Invalid user'));
+		}
+		$options = array('conditions' => array('User.' . $this->User->primaryKey => $id));
+		$this->set('user', $this->User->find('first', $options));
+	}
+public function admin_delete($id = null) {
+	$this->User->id = $id;
+		if (!$this->User->exists()) {
+			throw new NotFoundException(__('Invalid user'));
+		}
+		$this->request->allowMethod('post', 'delete');
+		if ($this->User->delete()) {
+			$this->Session->setFlash(__('The user has been deleted.'));
+		} else {
+			$this->Session->setFlash(__('The user could not be deleted. Please, try again.'));
+		}
+		return $this->redirect(array('action' => 'admin_index'));
+	}
+public function admin_edit($id=null) {
+	$info_user = $this->User->find('first',array(
+		'conditions'=>array('User.id'=>$id)
+		));
+	$this->set('info_user',$info_user);
+	if (!$this->User->exists($id)) {
+			throw new NotFoundException(__('Invalid user'));
+		}
+		if ($this->request->is(array('post', 'put'))) {
+			if ($this->User->save($this->request->data)) {
+				$this->Session->setFlash(__('Lưu thành công.','default',array('class'=>'alert alert-info'),'noteEdit'));
+				return $this->redirect(array('action' => 'admin_index'));
+			} else {
+				$this->Session->setFlash(__('The user could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('User.' . $this->User->primaryKey => $id));
+			$this->request->data = $this->User->find('first', $options);
+		}
+		$groups = $this->User->Group->find('list');
+		$this->set(compact('groups'));
+	}
 /**
  * view method
  *
--- View/Elements/admin/menu.ctp
+++ View/Elements/admin/menu.ctp
@@ -2,7 +2,7 @@
   <?php echo $this->Html->link('Quản lí phòng', array('controller' => 'rooms', 'action' => 'index'), array('class' => 'list-group-item list-group-item-success')); ?>
   <?php echo $this->Html->link('Thông tin đặt phòng', array('controller' => 'customers', 'action' => 'index'), array('class' => 'list-group-item list-group-item-success')); ?>
   <?php echo $this->Html->link('Tài khoản', array('controller' => 'users', 'action' => 'index'), array('class' => 'list-group-item list-group-item-success')); ?>
-  <?php echo $this->Html->link('Sản phẩm', array('controller' => 'servives', 'action' => 'index'), array('class' => 'list-group-item list-group-item-success')); ?>
+  <?php echo $this->Html->link('Sản phẩm', array('controller' => 'Services', 'action' => 'index'), array('class' => 'list-group-item list-group-item-success')); ?>
   <?php echo $this->Html->link('Thống kê', array('controller' => 'orders', 'action' => 'index'), array('class' => 'list-group-item list-group-item-success')); ?>
   <?php echo $this->Html->link('Liên hệ', '#', array('class' => 'list-group-item list-group-item-success')); ?>
 </div>
